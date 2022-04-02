420fd9ed7339dd75f6be85b6b5009dad98827913
higor.vieira <higor.vieira@opah.com.br>
2022-04-01 22:04:23 -0300

    page signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
-import { StatusBar } from 'react-native';
+
 import { SignIn } from './screens/SignIn';
 import theme from './theme';
 
